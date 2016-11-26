54469f862ec4d2b17033a4fb64bb46d452570d4e
Jeremy <jeremy.hoffman2016@gmail.com>
2016-11-25 19:19:18 -0800

    working buttons, editable items

--- content/todoapp.js
+++ content/todoapp.js
@@ -4,19 +4,20 @@ jQuery(document).ready(function($){
 	var exampleItemHtml = $('#example-item').html()
 
 	exampleItemHtml = "<div class='to-do-items'>" + exampleItemHtml + '</div>'
-	
-	console.log(exampleItemHtml)
-	
-	$('#complete-button').click(function(){
-		alert('clicked on Complete!')
-	})
 
-	$('#edit-button').click(function(){
-		alert('clicked on Edit!')
+
+
+	$('#complete-button').click(function(){
+		//crosses out selected items
+		$('.to-do-item-box:checked').siblings('.to-do-item-text').css('text-decoration','line-through')
+
 	})
 
 	$('#trash-button').click(function(){
-		alert('clicked on Trash!')
+		//removes out selected items
+		$('.to-do-item-box:checked').parent().remove()
+		
+		//
 	})
 
 	$('#add-button').click(function(){
@@ -24,5 +25,5 @@ jQuery(document).ready(function($){
 		var output = exampleItemHtml.replace('-EXAMPLE_ITEM1-', newItem)
 		$('#display').append(output)
 	})
-	
+
 })
