8fc4dbb6621f446daabb8f397935d84b4b9ddb18
Jeremy <jeremy.hoffman2016@gmail.com>
2016-11-25 20:23:36 -0800

    background changer

--- content/todoapp.js
+++ content/todoapp.js
@@ -5,8 +5,6 @@ jQuery(document).ready(function($){
 
 	exampleItemHtml = "<div class='to-do-items'>" + exampleItemHtml + '</div>'
 
-
-
 	$('#complete-button').click(function(){
 		//crosses out selected items
 		$('.to-do-item-box:checked').siblings('.to-do-item-text').css('text-decoration','line-through')
@@ -16,7 +14,7 @@ jQuery(document).ready(function($){
 	$('#trash-button').click(function(){
 		//removes out selected items
 		$('.to-do-item-box:checked').parent().remove()
-		
+
 		//
 	})
 
@@ -24,6 +22,25 @@ jQuery(document).ready(function($){
 		var newItem = $('#input-text-box').val()
 		var output = exampleItemHtml.replace('-EXAMPLE_ITEM1-', newItem)
 		$('#display').append(output)
+	})
+
+	//changing background colors
+	var colors = [
+		'#282c34',
+		'#ff0000',
+		'#482f3f',
+		'#60ee60',
+		'#cccccc'
+	]
+
+	var counter = 0
+		console.log ( colors )
+	$('#bkgrndbtn').click(function (){
+		counter = counter + 1
+		if (counter > colors.length - 1) {
+			counter = 0
+		}
+		$('body').css('background-color', colors[counter])
 	})
 
 })
